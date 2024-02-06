f0ff9753b1b5a0eb0360bcfe2f580d54568ffed9
Dylan Adams <dylancadams1@gmail.com>
2024-02-06 15:06:08 -0500

    Created the basic page layout

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -4,20 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Kadwa:wght@400;700&display=swap" rel="stylesheet" />
+      </head>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img className="logo" src="pokemon_logo.png" alt="Pokemon Logo" />
+        <h1 className="title">Generation 1 Pokédex</h1>
       </header>
+
+      <footer className="App-footer">
+
+      </footer>
+
     </div>
   );
 }
