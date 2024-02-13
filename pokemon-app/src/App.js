01ed9afa20dd497253abf0482fae19235f9c7bfa
Dylan Adams <dylancadams1@gmail.com>
2024-02-13 14:04:31 -0500

    Created working vertical carousel

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import {Component} from 'react';
-import VerticalCarousel from './VerticalCarousel';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Pokemon from './Pokemon';
 import Home from './Home';
--- pokemon-app/src/Home.js
+++ pokemon-app/src/Home.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import VerticalCarousel from './VerticalCarousel';
 import {config} from 'react-spring';
-
+import Carousel from 'react-elastic-carousel';
+import './Home.css';
 
 let slides = [
+  {
+    key: 0,
+    content: "0"
+  },
     {
       key: 1,
       content: "1"
@@ -14,60 +19,123 @@ let slides = [
     },
     {
       key: 3,
-      content: "2"
-    },
-    {
-      key: 4,
       content: "3"
     },
     {
-      key: 5,
+      key: 4,
       content: "4"
     },
     {
-      key: 6,
+      key: 5,
       content: "5"
     },
     {
-      key: 7,
+      key: 6,
       content: "6"
     },
     {
-      key: 8,
+      key: 7,
       content: "7"
+    },
+    {
+      key: 8,
+      content: "8"
     }
   ];
 
-export default class Home extends React.Component {
+  let activeKey = 0;
 
-    state = {
-        goToSlide: 0,
-        offsetRadius: 0,
-        showNavigation: true,
-        config: config.gentle
-      };
+  let itemArr = [];
+  var up = false;
+  var down = false;
+
+function Home() {
     
-    render() {
-        return (
-            <div
-                style={{
-                    position: "fixed",
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    width: "75vw",
-                    height: "75vh",
-                    margin: "0 auto",
-                    background: "#7FfFbF"
-                }}
-            >
-            <VerticalCarousel
-                slides={slides}
-                offsetRadius={this.state.offsetRadius}
-                showNavigation={this.state.showNavigation}
-                animationConfig={this.state.config}
-            />
-      </div>
-        )
+  const [activeItems, setActiveItems] = useState(slides.slice(activeKey, activeKey + 5));
+  const [moveDown, setMoveDown] = useState(false);
+  const [moveUp, setMoveUp] = useState(false);
+  
+
+  const downAction = () => {
+    if (activeKey < slides.length-3) {
+      setMoveDown(true);
+      down = true;
+      // activeKey++;
+      // setActiveItems(slides.slice(activeKey, activeKey + 4));
+      
+      // console.log(activeItems);
     }
+  }
+
+  const upAction = () => {
+    if (activeKey > 0) {
+      setMoveUp(true);
+      up = true;
+      // activeKey--;
+      // setActiveItems(slides.slice(activeKey, activeKey + 4));
+     
+      // console.log(activeItems);
+    }
+  }
+
+  const onEnd = () => {
+    setMoveUp(false);
+    setMoveDown(false);
+  }
+
+  useEffect(() => {
+    if (!moveUp && up) {
+      console.log('up');
+      if (activeKey > 0) {
+        activeKey--;
+        setActiveItems(slides.slice(activeKey, activeKey + 5));
+      }
+      up = false;
+    } else if (!moveDown && down){
+      if (activeKey < slides.length-3) {
+        activeKey++;
+        setActiveItems(slides.slice(activeKey, activeKey + 5));
+      }
+      down = false;
+    } 
+  }, [moveUp, moveDown])
+
+  return (
+    <>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Kadwa:wght@400;700&display=swap" rel="stylesheet" />
+      </head>
+
+      <div className="App">
+        <header className="App-header">
+          <img className="logo" src="pokemon_logo.png" alt="Pokemon logo" />
+          <h1 className="title">Generation 1 Pokedex</h1>
+        </header>
+
+        <div className="carousel">
+          <ul>
+            {activeItems.map(item => <li className={moveDown ? "item-movedown" : moveUp ? "item-moveup" : "item"} key={item.key} onAnimationEnd={() => onEnd()}>{item.content}</li>)}
+          </ul>
+            
+            <button onClick={() => {upAction()}}>Up</button>
+            <button onClick={() => {downAction()}}>Down</button>
+        </div>
+
+        <footer className="App-footer">
+
+        </footer>
+      </div>
+
+        
+    </>
+  );
 }
+
+export default Home;
+
+
+
+
+
